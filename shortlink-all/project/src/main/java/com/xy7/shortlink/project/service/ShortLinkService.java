674d38a5044e1c2445f1f5f3e2ad95f0d329e431
xy7GG <1665961512@qq.com>
2024-05-09 22:02:55 +0800

    开发短链接分页查询功能

--- shortlink-all/project/src/main/java/com/xy7/shortlink/project/service/ShortLinkService.java
+++ shortlink-all/project/src/main/java/com/xy7/shortlink/project/service/ShortLinkService.java
@@ -1,9 +1,12 @@
 package com.xy7.shortlink.project.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.xy7.shortlink.project.dao.entity.ShortLinkDO;
 import com.xy7.shortlink.project.dto.req.ShortLinkCreateReqDTO;
+import com.xy7.shortlink.project.dto.req.ShortLinkPageReqDTO;
 import com.xy7.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
+import com.xy7.shortlink.project.dto.resp.ShortLinkPageRespDTO;
 
 /**
  * 短链接接口层
@@ -17,5 +20,13 @@ public interface ShortLinkService extends IService<ShortLinkDO> {
      * @return 短链接创建信息
      */
     ShortLinkCreateRespDTO createShortLink(ShortLinkCreateReqDTO requestParam);
+
+    /**
+     * 分页查询短链接
+     *
+     * @param requestParam 分页查询短链接请求参数
+     * @return 短链接分页返回结果
+     */
+    IPage<ShortLinkPageRespDTO> pageShortLink(ShortLinkPageReqDTO requestParam);
 }
 
