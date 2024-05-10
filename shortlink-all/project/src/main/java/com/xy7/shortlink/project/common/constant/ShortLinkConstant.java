ba0f33d71c348e21d0320146933e602443d5d571
xy7GG <1665961512@qq.com>
2024-05-10 12:44:52 +0800

    短链接跳转时如果已过有效期设置为空

--- shortlink-all/project/src/main/java/com/xy7/shortlink/project/common/constant/ShortLinkConstant.java
+++ shortlink-all/project/src/main/java/com/xy7/shortlink/project/common/constant/ShortLinkConstant.java
@@ -6,7 +6,7 @@ package com.xy7.shortlink.project.common.constant;
 public final class ShortLinkConstant {
 
     /**
-     * 永久短链接默认缓存有效时间
+     * 永久短链接默认缓存有效时间,默认一个月
      */
     public static final long DEFAULT_CACHE_VALID_TIME = 2626560000L;
 }
