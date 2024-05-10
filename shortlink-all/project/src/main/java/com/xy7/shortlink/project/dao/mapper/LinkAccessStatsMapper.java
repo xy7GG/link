ee9a0b9d506e08733883c2d53368240a385a9e17
xy7GG <1665961512@qq.com>
2024-05-10 21:09:13 +0800

    开发短链接监控之基础访问UV数据

--- shortlink-all/project/src/main/java/com/xy7/shortlink/project/dao/mapper/LinkAccessStatsMapper.java
+++ shortlink-all/project/src/main/java/com/xy7/shortlink/project/dao/mapper/LinkAccessStatsMapper.java
@@ -9,15 +9,12 @@ import org.apache.ibatis.annotations.Param;
  * 短链接基础访问监控持久层
  */
 public interface LinkAccessStatsMapper extends BaseMapper<LinkAccessStatsDO> {
-
     /**
      * 记录基础访问监控数据
      */
-    @Insert("INSERT INTO t_link_access_stats (full_short_url, gid, date, pv, uv, uip, hour, weekday, create_time, update_time, del_flag) " +
-            "VALUES( #{linkAccessStats.fullShortUrl}, #{linkAccessStats.gid}, #{linkAccessStats.date}, #{linkAccessStats.pv}, #{linkAccessStats.uv}, #{linkAccessStats.uip}, #{linkAccessStats.hour}, #{linkAccessStats.weekday}, NOW(), NOW(), 0) " +
-            "ON DUPLICATE KEY UPDATE " +
-            "pv = pv + #{linkAccessStats.pv}, " +
+    @Insert("INSERT INTO t_link_access_stats (full_short_url, date, pv, uv, uip, hour, weekday, create_time, update_time, del_flag) " +
+            "VALUES( #{linkAccessStats.fullShortUrl}, #{linkAccessStats.date}, #{linkAccessStats.pv}, #{linkAccessStats.uv}, #{linkAccessStats.uip}, #{linkAccessStats.hour}, #{linkAccessStats.weekday}, NOW(), NOW(), 0) ON DUPLICATE KEY UPDATE pv = pv +  #{linkAccessStats.pv}, " +
             "uv = uv + #{linkAccessStats.uv}, " +
-            "uip = uip + #{linkAccessStats.uip};")
+            " uip = uip + #{linkAccessStats.uip};")
     void shortLinkStats(@Param("linkAccessStats") LinkAccessStatsDO linkAccessStatsDO);
 }
