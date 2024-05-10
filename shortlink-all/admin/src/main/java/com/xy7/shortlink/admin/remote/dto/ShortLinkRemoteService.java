9697b56b1432da23172f38e081bc56839a057e6f
xy7GG <1665961512@qq.com>
2024-05-10 14:59:31 +0800

    开发短链接新增回收站功能

--- shortlink-all/admin/src/main/java/com/xy7/shortlink/admin/remote/dto/ShortLinkRemoteService.java
+++ shortlink-all/admin/src/main/java/com/xy7/shortlink/admin/remote/dto/ShortLinkRemoteService.java
@@ -5,10 +5,7 @@ import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.TypeReference;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.xy7.shortlink.admin.common.convention.result.Result;
-import com.xy7.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
-import com.xy7.shortlink.admin.remote.dto.req.ShortLinkGroupCountQueryRespDTO;
-import com.xy7.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
-import com.xy7.shortlink.admin.remote.dto.req.ShortLinkUpdateReqDTO;
+import com.xy7.shortlink.admin.remote.dto.req.*;
 import com.xy7.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
 import com.xy7.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -84,5 +81,15 @@ public interface ShortLinkRemoteService {
         String resultStr = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/title?url=" + url);
         return JSON.parseObject(resultStr, new TypeReference<>() {
         });
+    }
+
+
+    /**
+     * 保存回收站
+     *
+     * @param requestParam 请求参数
+     */
+    default void saveRecycleBin(RecycleBinSaveReqDTO requestParam) {
+        HttpUtil.post("http://127.0.0.1:8001/api/short-link/v1/recycle-bin/save", JSON.toJSONString(requestParam));
     }
 }
