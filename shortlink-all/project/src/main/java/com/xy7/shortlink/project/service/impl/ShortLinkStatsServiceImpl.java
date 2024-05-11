032771cf25b9057b169d87ffc32c457f064a7e6e
xy792 <1665961512@qq.com>
2024-05-12 00:43:06 +0800

    修复短链接访问记录中的访客类型字段展示错误

--- shortlink-all/project/src/main/java/com/xy7/shortlink/project/service/impl/ShortLinkStatsServiceImpl.java
+++ shortlink-all/project/src/main/java/com/xy7/shortlink/project/service/impl/ShortLinkStatsServiceImpl.java
@@ -419,7 +419,7 @@ public class ShortLinkStatsServiceImpl implements ShortLinkStatsService {
             String uvType = uvTypeList.stream()
                     .filter(item -> Objects.equals(each.getUser(), item.get("user")))
                     .findFirst()
-                    .map(item -> item.get("UvType"))
+                    .map(item -> item.get("uvType"))
                     .map(Object::toString)
                     .orElse("旧访客");
             each.setUvType(uvType);
